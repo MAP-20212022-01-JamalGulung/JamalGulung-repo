7b0bc424ddb6d0d22a0ab354202041cd3154c0bf
Firhish <86069053+Firhish@users.noreply.github.com>
2022-05-30 23:19:44 +0800

    Schedule and Home UI
    - Responsive calendar
    - Dummy operating hours
    - Complete Ui

---
+++ qidgym_management_system/lib/services/world_time.dart
@@ -0,0 +1,27 @@
+import 'package:intl/intl.dart';
+
+// DateFormat dateFormat = DateFormat("yyyy-MM-dd HH:mm:ss");
+dynamic currentDay = DateFormat('EEEE').format(DateTime.now());
+dynamic currentDate = DateFormat('d').format(DateTime.now());
+
+// Future<String> getDay() async {
+//   String day = "";
+
+//   if (currentTime == "Sunday") {
+//     day = "Sun";
+//   } else if (currentTime == "Monday") {
+//     day = "Mon";
+//   } else if (currentTime == "Tuesday") {
+//     day = "Tue";
+//   } else if (currentTime == "Wednesday") {
+//     day = "Wed";
+//   } else if (currentTime == "Thursday") {
+//     day = "Thu";
+//   } else if (currentTime == "Friday") {
+//     day = "Fri";
+//   } else if (currentTime == "Saturday") {
+//     day = "Sat";
+//   }
+
+//   return day;
+// }
