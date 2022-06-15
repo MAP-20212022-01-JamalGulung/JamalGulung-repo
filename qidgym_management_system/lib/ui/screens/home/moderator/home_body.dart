2e423f266b4841711691c130f0402d893fbb0936
Firhish <86069053+Firhish@users.noreply.github.com>
2022-06-15 23:07:18 +0800

    Moderator edit button (home)
    - button to edit schedule & promo

--- qidgym_management_system/lib/ui/screens/home/moderator/home_body.dart
+++ qidgym_management_system/lib/ui/screens/home/moderator/home_body.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 // import 'package:loading_indicator/loading_indicator.dart';
 import 'package:map_mvvm/map_mvvm.dart';
 import 'package:qidgym_management_system/ui/screens/home/moderator/home_viewmodal.dart';
+import 'package:qidgym_management_system/ui/screens/home/moderator/home_screen.dart';
+import 'package:qidgym_management_system/services/world_time.dart';
 
 class HomeModeratorBody extends StatefulWidget {
   const HomeModeratorBody({Key? key}) : super(key: key);
@@ -12,6 +15,8 @@ class HomeModeratorBody extends StatefulWidget {
 }
 
 class _HomeModeratorBody extends State<HomeModeratorBody> {
+  final CollectionReference _operation =
+      FirebaseFirestore.instance.collection("OperationHour");
   @override
   void initState() {
     super.initState();
@@ -19,36 +24,312 @@ class _HomeModeratorBody extends State<HomeModeratorBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
-      alignment: Alignment.center,
-      child: Container(
-        child: View<HomeModeratorViewModel>(
-          builder: (_, viewmodel) => FutureBuilder<Map>(
-              future: viewmodel.getOperationHour("Sunday"),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  Map<String, dynamic> data =
-                      snapshot.data as Map<String, dynamic>;
-                  Text(data['start']);
-                }
-                return Column(
-                  children: [
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text("Loading")
-                    // LoadingIndicator(
-                    //     indicatorType: Indicator.ballClipRotatePulse,
-                    //     colors: const [Color.fromRGBO(238, 29, 82, 1)],
-                    //     strokeWidth: 2,
-                    //     backgroundColor: Color.fromRGBO(24, 24, 24, 1),
-                    //     pathBackgroundColor: Color.fromRGBO(24, 24, 24, 1)),
-                  ],
-                );
-              }),
-        ),
-      ),
+    return View<HomeModeratorViewModel>(
+      builder: (_, viewmodel) => FutureBuilder<Map>(
+          future: viewmodel.getOperationHour("Sunday"),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              Map<String, dynamic> data = snapshot.data as Map<String, dynamic>;
+              Text(data['start']);
+            }
+            return SingleChildScrollView(
+              child: Padding(
+                padding:
+                    EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 20),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 120,
+                            height: 120,
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(40, 40, 40, 1)),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    getDay(),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 26,
+                                        color: Colors.white),
+                                  ),
+                                  Text(
+                                    "$currentDate",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w800,
+                                        fontSize: 34,
+                                        color: Colors.white),
+                                  ),
+                                ]),
+                          ),
+                          GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                "Edit Schedule",
+                                style: TextStyle(
+                                    color: Color.fromRGBO(238, 29, 82, 1)),
+                              ))
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      FutureBuilder<DocumentSnapshot<Object?>>(
+                          future: _operation.doc(currentDay).get(),
+                          // .where('day', isEqualTo: currentDay)
+                          // .snapshots(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasError) {
+                              return Text(
+                                'Something went wrong',
+                                style: TextStyle(color: Colors.white),
+                              );
+                            }
+
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Text("Loading",
+                                  style: TextStyle(color: Colors.white));
+                            }
+                            Map<String, dynamic> data =
+                                snapshot.data!.data() as Map<String, dynamic>;
+
+                            // return ListView(
+                            //     children: snapshot.data!.docs
+                            //         .map((DocumentSnapshot document) {
+                            //   data = document.data as Map<String, dynamic>;
+                            //   List<String> strArr = [];
+                            //   strArr = List.from(data!['day']);
+
+                            return Row(
+                              children: [
+                                Icon(
+                                  Icons.access_time,
+                                  color: Colors.white,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "OPEN HOURS from ${data['start']} to ${data['end']}",
+                                  //"hehe",
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            );
+
+                            // }).toList());
+                          }),
+
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Promotion",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w800,
+                                fontSize: 24),
+                          ),
+                          GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                "Edit Promotion",
+                                style: TextStyle(
+                                  color: Color.fromRGBO(238, 29, 82, 1),
+                                ),
+                              ))
+                        ],
+                      ),
+                      Text(
+                        "Recently Added",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+
+                      PromotionBox(context),
+
+                      // ListView(
+                      //   physics: NeverScrollableScrollPhysics(),
+                      //   scrollDirection: Axis.vertical,
+                      //   shrinkWrap: true,
+                      //   children: [
+                      //     GestureDetector(
+                      //       onTap: () {
+                      //         Navigator.push(
+                      //           context,
+                      //           MaterialPageRoute(
+                      //               builder: (context) => const ViewPromo()),
+                      //         );
+                      //       },
+                      //       child: Container(
+                      //         width: double.infinity,
+                      //         height: 150,
+                      //         decoration: BoxDecoration(
+                      //             image: DecorationImage(
+                      //                 image: AssetImage("assets/blue.jpg"),
+                      //                 fit: BoxFit.cover),
+                      //             borderRadius: BorderRadius.circular(10)),
+                      //         child: Stack(children: [
+                      //           Container(
+                      //             decoration: BoxDecoration(
+                      //                 color: Color.fromRGBO(158, 27, 117, 0.7),
+                      //                 borderRadius: BorderRadius.circular(10)),
+                      //           ),
+                      //           Column(
+                      //             mainAxisAlignment: MainAxisAlignment.end,
+                      //             children: [
+                      //               Padding(
+                      //                 padding: EdgeInsets.all(12.0),
+                      //                 child: Text(
+                      //                   "Summer Body Challenge",
+                      //                   style: TextStyle(
+                      //                       color: Colors.white, fontSize: 20.0),
+                      //                 ),
+                      //               )
+                      //             ],
+                      //           )
+                      //         ]),
+                      //       ),
+                      //     ),
+
+                      //   ],
+                      // )
+
+                      // ListView(
+                      //   scrollDirection: Axis.horizontal,
+                      //   children: <Widget>[],
+                      // )
+
+                      //   Row(
+                      //     mainAxisSize: MainAxisSize.max,
+                      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      //     children: [
+                      //     GestureDetector(
+                      //         onTap: () {Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(builder: (context) => const ViewPromo()),
+                      // );
+                      //         },
+                      //         child: Container(
+                      //           // decoration: BoxDecoration(color: Color.fromRGBO(158, 27, 117, 0.5),borderRadius: BorderRadius.circular(10)),
+                      //           decoration: BoxDecoration(
+                      //               image: DecorationImage(
+                      //                   image: AssetImage("assets/pink.jpg"),
+                      //                   fit: BoxFit.cover),
+                      //               borderRadius: BorderRadius.circular(10)),
+                      //           height: 280,
+                      //           width: 160,
+                      //           child: Stack(children: [
+                      //             Container(
+                      //               decoration: BoxDecoration(
+                      //                   color: Color.fromRGBO(158, 27, 117, 0.6),
+                      //                   borderRadius: BorderRadius.circular(10)),
+                      //             ),
+                      //             Column(
+                      //               mainAxisAlignment: MainAxisAlignment.end,
+                      //               children: [
+                      //                 Padding(
+                      //                   padding: const EdgeInsets.all(8.0),
+                      //                   child: Text(
+                      //                     "Summer Body Challenge",
+                      //                     style: TextStyle(
+                      //                         color: Colors.white, fontSize: 18),
+                      //                   ),
+                      //                 )
+                      //               ],
+                      //             )
+                      //           ]),
+                      //         ),
+                      //       ),
+                      //       Column(
+                      //         mainAxisSize: MainAxisSize.max,
+                      //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      //         children: [
+                      //           Container(
+                      //             // decoration: BoxDecoration(color: Color.fromRGBO(27, 117, 158, 0.5),borderRadius: BorderRadius.circular(10)),
+                      //             decoration: BoxDecoration(
+                      //                 image: DecorationImage(
+                      //                     image: AssetImage("assets/blue.jpg"),
+                      //                     fit: BoxFit.cover),
+                      //                 borderRadius: BorderRadius.circular(10)),
+                      //             height: 134,
+                      //             width: 160,
+
+                      //             child: Stack(children: [
+                      //               Container(
+                      //                 decoration: BoxDecoration(
+                      //                     color: Color.fromRGBO(27, 117, 158, 0.6),
+                      //                     borderRadius: BorderRadius.circular(10)),
+                      //               ),
+                      //               Column(
+                      //                 mainAxisAlignment: MainAxisAlignment.end,
+                      //                 children: [
+                      //                   Padding(
+                      //                     padding: const EdgeInsets.all(8.0),
+                      //                     child: Text(
+                      //                       "Merry May",
+                      //                       style: TextStyle(
+                      //                           color: Colors.white, fontSize: 18),
+                      //                     ),
+                      //                   )
+                      //                 ],
+                      //               )
+                      //             ]),
+                      //           ),
+                      //           SizedBox(
+                      //             height: 12,
+                      //           ),
+                      //           Container(
+                      //             // decoration: BoxDecoration(color: Color.fromRGBO(117, 158, 27, 0.5),borderRadius: BorderRadius.circular(10)),
+                      //             decoration: BoxDecoration(
+                      //                 image: DecorationImage(
+                      //                     image: AssetImage("assets/yellow.jpg"),
+                      //                     fit: BoxFit.cover),
+                      //                 borderRadius: BorderRadius.circular(10)),
+                      //             height: 134,
+                      //             width: 160,
+
+                      //             child: Stack(children: [
+                      //               Container(
+                      //                 decoration: BoxDecoration(
+                      //                     color: Color.fromRGBO(117, 158, 27, 0.6),
+                      //                     borderRadius: BorderRadius.circular(10)),
+                      //               ),
+                      //               Column(
+                      //                 mainAxisAlignment: MainAxisAlignment.end,
+                      //                 children: [
+                      //                   Padding(
+                      //                     padding: const EdgeInsets.all(8.0),
+                      //                     child: Text(
+                      //                       "Zumba Raya",
+                      //                       style: TextStyle(
+                      //                           color: Colors.white, fontSize: 18),
+                      //                     ),
+                      //                   )
+                      //                 ],
+                      //               )
+                      //             ]),
+                      //           ),
+                      //         ],
+                      //       )
+                      //     ],
+                      //   ),
+                    ]),
+              ),
+            );
+          }),
     );
   }
 }
--- qidgym_management_system/lib/ui/screens/home/moderator/home_screen.dart
+++ qidgym_management_system/lib/ui/screens/home/moderator/home_screen.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:flutter/material.dart";
 import 'home.dart';
+import 'package:qidgym_management_system/services/world_time.dart';
+import 'package:qidgym_management_system/ui/screens/promotion/customer/viewpromotion.dart';
+import 'dart:math' as math;
+
+Map<String, dynamic>? data;
 
 class HomeModerator extends StatefulWidget {
   const HomeModerator({Key? key}) : super(key: key);
@@ -9,6 +15,7 @@ class HomeModerator extends StatefulWidget {
 }
 
 class _HomeModerator extends State<HomeModerator> {
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -19,4 +26,179 @@ class _HomeModerator extends State<HomeModerator> {
           body: HomeModeratorBody()),
     );
   }
+}
+
+String getDay() {
+  String day = "";
+
+  if (currentDay == "Sunday") {
+    day = "Sun";
+  } else if (currentDay == "Monday") {
+    day = "Mon";
+  } else if (currentDay == "Tuesday") {
+    day = "Tue";
+  } else if (currentDay == "Wednesday") {
+    day = "Wed";
+  } else if (currentDay == "Thursday") {
+    day = "Thu";
+  } else if (currentDay == "Friday") {
+    day = "Fri";
+  } else if (currentDay == "Saturday") {
+    day = "Sat";
+  }
+
+  return day;
+}
+
+Widget PromotionBox(BuildContext context) {
+  return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('Promotion').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return ListView(
+          physics: NeverScrollableScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          children: snapshot.data!.docs.map((DocumentSnapshot document){
+            data = document.data() as Map<String, dynamic>;
+
+            return Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) =>  ViewPromo(docId: document.id,)),
+                    );
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 125,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(data!['pic']),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Stack(children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Text(
+                              data!['title'],
+                              style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w600),
+                            ),
+                          )
+                        ],
+                      )
+                    ]),
+                  ),
+                  
+                ),
+                SizedBox(height: 8,),
+              ],
+              
+            );
+            
+
+
+          }).toList(),
+
+
+          // [
+            // GestureDetector(
+            //   onTap: () {
+            //     Navigator.push(
+            //       context,
+            //       MaterialPageRoute(builder: (context) => const ViewPromo()),
+            //     );
+            //   },
+            //   child: Container(
+            //     width: double.infinity,
+            //     height: 150,
+            //     decoration: BoxDecoration(
+            //         image: DecorationImage(
+            //             image: AssetImage("assets/blue.jpg"),
+            //             fit: BoxFit.cover),
+            //         borderRadius: BorderRadius.circular(10)),
+            //     child: Stack(children: [
+            //       Container(
+            //         decoration: BoxDecoration(
+            //             color: Color.fromRGBO(158, 27, 117, 0.7),
+            //             borderRadius: BorderRadius.circular(10)),
+            //       ),
+            //       Column(
+            //         mainAxisAlignment: MainAxisAlignment.end,
+            //         children: [
+            //           Padding(
+            //             padding: EdgeInsets.all(12.0),
+            //             child: Text(
+            //               "Summer Body Challenge",
+            //               style: TextStyle(color: Colors.white, fontSize: 20.0),
+            //             ),
+            //           )
+            //         ],
+            //       )
+            //     ]),
+            //   ),
+            // ),
+          // ],
+        );
+      });
+
+  // ListView(
+  //   physics: NeverScrollableScrollPhysics(),
+  //   scrollDirection: Axis.vertical,
+  //   shrinkWrap: true,
+  //   children: [
+  //     GestureDetector(
+  //       onTap: () {
+  //         Navigator.push(
+  //           context,
+  //           MaterialPageRoute(builder: (context) => const ViewPromo()),
+  //         );
+  //       },
+  //       child: Container(
+  //         width: double.infinity,
+  //         height: 150,
+  //         decoration: BoxDecoration(
+  //             image: DecorationImage(
+  //                 image: AssetImage("assets/blue.jpg"), fit: BoxFit.cover),
+  //             borderRadius: BorderRadius.circular(10)),
+  //         child: Stack(children: [
+  //           Container(
+  //             decoration: BoxDecoration(
+  //                 color: Color.fromRGBO(158, 27, 117, 0.7),
+  //                 borderRadius: BorderRadius.circular(10)),
+  //           ),
+  //           Column(
+  //             mainAxisAlignment: MainAxisAlignment.end,
+  //             children: [
+  //               Padding(
+  //                 padding: EdgeInsets.all(12.0),
+  //                 child: Text(
+  //                   "Summer Body Challenge",
+  //                   style: TextStyle(color: Colors.white, fontSize: 20.0),
+  //                 ),
+  //               )
+  //             ],
+  //           )
+  //         ]),
+  //       ),
+  //     ),
+  //   ],
+  // );
 }
