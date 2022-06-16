8c80630019e40773a1262a6ae21d073c65befcfa
khainorazam <khainorazam25@gmail.com>
2022-06-16 11:23:51 +0800

    Update booking_moderator_service_mock.dart

--- qidgym_management_system/lib/services/booking/booking_moderator_service_mock.dart
+++ qidgym_management_system/lib/services/booking/booking_moderator_service_mock.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:qidgym_management_system/services/booking/booking_moderator_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../model/booking.dart';
 
